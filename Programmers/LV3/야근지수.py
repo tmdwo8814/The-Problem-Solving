f0ff6b5d57d606e30523ba708f0fb2b6adf05b20
kimsj00 <tmdwo8814@gmail.com>
2023-12-27 17:10:25 +0900

    ADD 231227

--- Programmers/LV3/야근지수.py
+++ Programmers/LV3/야근지수.py
@@ -4,22 +4,40 @@ def solution(n, works):
         works.sort()
 
         max_count = works.count(max(works))
-        gap = max(works) - works[-(max_count + 1)]
+        if max(works) == min(works):
+            gap = 0
+        else:
+            gap = max(works) - works[-(max_count + 1)]
 
-        if max_count > n:
+        if sum(works) <= n:
+            return 0
+        
+        elif max_count > n:
             for i in range(1, n+1):
                 works[-i] -= 1
                 n -= 1
+
+        elif min(works) == max(works):
+                if max_count * max(works) > n:
+                    minus = n // max_count
+                    for i in range(1, max_count):
+                        works[-i] -= minus
+                        n -= minus
+
+            
         else:
             for i in range(gap, 1, -1):
                 if gap * max_count > n:
                     continue
                 elif gap * max_count <= n:
-                    for j in range(1, max_count+1):
+                    for j in range(1, max_count):
                         works[-j] -= i
                         n -= i
                     break
 
+    
+    for i in works:
+        answer += i*i
 
     return answer
 
